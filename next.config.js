8dc8a57fc6d8ac253de4ea9e016132a959160973
richosj <44463951+richosj@users.noreply.github.com>
2024-10-04 01:18:11 +0900

    d

--- next.config.js
+++ next.config.js
@@ -3,10 +3,10 @@ const nextConfig = {
   output: 'export',  // 정적 사이트로 내보내기 설정
   images: {
     unoptimized: true,  // 이미지 최적화 비활성화
-    
   },
   basePath: '/my-website',  // GitHub Pages 경로
   trailingSlash: true,  // 모든 URL에 슬래시 추가
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/my-website/' : '',  // 정적 파일 경로 설정
 }
 
 module.exports = nextConfig;
