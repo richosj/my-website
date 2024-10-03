bcec84f428f8aadc81ef2529aab3092d607c637d
richosj <44463951+richosj@users.noreply.github.com>
2024-10-04 01:51:01 +0900

    '11'

--- src/app/components/Sidebar/Sidebar.tsx
+++ src/app/components/Sidebar/Sidebar.tsx
@@ -7,8 +7,8 @@ const Sidebar = () => {
         <nav className="sidebar">
             <ul className="flex gap-4">
                 <li><Link href="/">홈</Link></li>
-                <li><Link href="/projects/">작업물</Link></li>
-                <li><Link href="/lib">자료</Link></li>
+                <li><Link href="/projects/">작업1111111111111물</Link></li>
+                <li><Link href="/lib">자료1122222222222222222</Link></li>
             </ul>
         </nav>
     )
